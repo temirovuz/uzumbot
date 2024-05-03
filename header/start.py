845d2ca3896b90c1c53dd17cb9c7a86cd2ece61c
temirovuz <temirov-muhammad@mail.ru>
2024-05-03 14:16:52 +0500

    start command add

--- header/start.py
+++ header/start.py
@@ -1,3 +1,9 @@
 from aiogram import types, Router
+from aiogram.filters import CommandStart
 
 router = Router()
+
+
+@router.message(CommandStart())
+async def start_bot(message: types.Message):
+    await message.edit(content="Salom test sifatida bot ishlamoqda bu vaqtincha hammasi!\nHammasi yaxshilikka")
