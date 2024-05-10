438ddae9d613ddb354182c7f60244b8539b58fc9
temirovuz <temirov-muhammad@mail.ru>
2024-05-10 11:09:48 +0500

    alembic upgrade add database

--- config.py
+++ config.py
@@ -2,3 +2,17 @@ from dotenv import dotenv_values
 
 envfile = dotenv_values(".env")
 bot_token = envfile['TOKEN']
+
+lang_text = 'Select a language\n\nВыберите языкn\n\nTilni tanglang'
+
+
+def uz_text(message):
+    return f'👋Assalomu alaykum <b>{message.from_user.full_name}</b>\n\n🔹Sizga kerakli bo\'limni tanlang'
+
+
+def en_text(message):
+    return f'👋Hello, <b>{message.from_user.full_name}</b>\n\n🔹Choose the section you need'
+
+
+def ru_text(message):
+    return f'👋Здравствуйте, <b>{message.from_user.full_name}</b>\n\n🔹Выберите нужный вам раздел'
