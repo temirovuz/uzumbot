4f2b311e6b9195aec40feb59ff39baba39a97515
temirovuz <temirov-muhammad@mail.ru>
2024-05-11 11:42:40 +0500

    add location, user, language

--- database/db.py
+++ database/db.py
@@ -1,17 +1,9 @@
 from .database import Base, Session, User
 
 
-def get_db():
-    db = Session()
-    try:
-        yield db
-    except:
-        db.close()
-
-
 # ------------------------------------->  User <-------------------------------------------#
-def create_user(user_id):
-    db = get_db()
+def add_user(user_id):
+    db = Session()
     user = User(user_id=user_id)
     db.add(user)
     db.commit()
@@ -19,14 +11,45 @@ def create_user(user_id):
     return user
 
 
-def get_user(user_id, db: get_db()):
+def get_user(user_id):
+    db = Session()
     user = db.query(User).filter(User.user_id == user_id).first()
     return user if user else False
 
 
-def add_location(user_id, lat, lon, db: get_db()):
-    user = get_user(user_id, db)
+# -----------------------> Location <----------------------------#
+def add_location(user_id, lat, lon):
+    db = Session()
+    user = get_user(user_id)
     if user:
         user.latitude = lat
         user.longitude = lon
         db.commit()
+    else:
+        return False
+
+
+def get_location(user_id):
+    db = Session()
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if user:
+        return user.latitude, user.longitude
+    else:
+        return False
+
+
+# -------------------> Language <-----------------------------#
+def get_lang(user_id):
+    db = Session()
+    user = db.query(User).filter(User.user_id == user_id).first()
+    return user.lang if user else False
+
+
+def add_lang(user_id, lang):
+    db = Session()
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if user:
+        user.lang = lang
+        db.commit()
+    else:
+        return False
